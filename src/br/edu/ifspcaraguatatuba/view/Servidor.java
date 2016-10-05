2ec79c075f18baa45e43071bae37bb81ef94bc0d
Caio <caiof.adriano@hotmail.com>
2016-10-05 13:26:37 -0300

    created panel to receive the client message.

--- src/br/edu/ifspcaraguatatuba/view/Servidor.java
+++ src/br/edu/ifspcaraguatatuba/view/Servidor.java
@@ -4,15 +4,16 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
-
-import br.edu.ifspcaraguatatuba.socket_servidor.Socket_Servidor;
 
 public class Servidor extends JFrame {
 
@@ -20,6 +21,7 @@ public class Servidor extends JFrame {
 	private JPanel contentPane;
 	
 	private JLabel lblStatusServidor;
+	private JTextPane textPane;
 
 	public Servidor() {
 		setTitle("Servidor");
@@ -34,17 +36,46 @@ public class Servidor extends JFrame {
 		btnConectar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				Socket_Servidor servidor = new Socket_Servidor();
 				try {
-					servidor.connect();
+					
+					ServerSocket servidor = new ServerSocket(12345);
+					
+					Runnable r = new Runnable() {
+						
+						@Override
+						public void run() {
+							
+							try {
+								
+								Socket cliente = servidor.accept();
+								Scanner entrada = new Scanner(cliente.getInputStream());
+								System.err.println("executando thread");
+								
+								String messages = "";
+								
+								while (entrada.hasNextLine()) {
+									messages += "Cliente: " + entrada.nextLine() + "\n";
+									textPane.setText(messages);
+								}
+								
+								entrada.close();
+								servidor.close();
+							}catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					};
+					
+					
+					Thread thread = new Thread(r);
+					thread.setPriority(Thread.MAX_PRIORITY);
+					thread.start();
 					
 					btnConectar.setEnabled(false);
-					lblStatusServidor.setForeground(Color.green);
 					lblStatusServidor.setText("Conectado");
+					lblStatusServidor.setForeground(Color.green);
 					
-					Thread thread = new Thread(servidor);
-					thread.start();
-				} catch (IOException e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				
@@ -62,5 +93,10 @@ public class Servidor extends JFrame {
 		lblStatusServidor.setForeground(Color.RED);
 		lblStatusServidor.setBounds(64, 13, 111, 14);
 		contentPane.add(lblStatusServidor);
+		
+		textPane = new JTextPane();
+		textPane.setEditable(false);
+		textPane.setBounds(10, 81, 414, 135);
+		contentPane.add(textPane);
 	}
 }
